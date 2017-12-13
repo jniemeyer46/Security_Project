4e2e4f733f9161dba9a378d89a27565fa4841b66
jniemeyer46 <jjniemeyer46@gmail.com>
2017-12-12 22:23:51 -0600

    yup

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main()
 	cout << "3. Enter the name of the file that contains Alice’s plaintext message: " << endl;
 	cin >> messageFile;
 
-	fin.open(messageFile);
+	fin.open("\"" + messageFile + "\"");
 	getline(fin, message);
 	fin.close();
 
